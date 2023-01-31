20a445c2e774b1fdbcfe060931a5307bba778c18
Ritik Rajdev <ritik_rajdev@mckinsey.com>
2023-01-31 15:40:36 +0530

    chore: remove uuid

--- src/utils/uuidUtils.js
+++
@@ -1,8 +0,0 @@
-function isUuidV4(string) {
-  const uuidRegexExp = /^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/gi;
-  return uuidRegexExp.test(string);
-} 
-
-module.exports = {
-  isUuidV4,
-};
